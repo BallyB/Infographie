41350558b8f2a63a6babb04dbb1f3b8a68c749f6
BallyB <ajeanne25@gmail.com>
2016-02-02 09:55:30 +0100

    LineSweep Bresenham

--- main.cpp
+++ main.cpp
@@ -24,11 +24,11 @@ Cx Cy
 
 on a notre point qu'on test Px Py
 je cherche u,v tel que P = (1-u-v)A + uB + vC)
-cette équation vient du repere cartésien
+cette Ã©quation vient du repere cartÃ©sien
 Px = (1-u-v)Ax+ uBx + vCx
 Py = (1-u-v)Ay+ uBy + vCy
 On parcourt tout les pixels de l'image,
-On regarde les coordonnées barycentrique de ce pixel par rapport au triangle,
+On regarde les coordonnÃ©es barycentrique de ce pixel par rapport au triangle,
 si au moin un pixel est negatif alors on jette pixel
 
 
@@ -143,7 +143,99 @@ void getLineNumberv(ifstream &myFile,std::vector<char> &tabvx, std::vector<char>
   myFile.close();
 }
 
+void barymetricFullMethod(int Ax, int Ay, int Az, int Bx, int By, int Bz, int Cx, int Cy, int Cz){
 
+    int starty = std::max(std::max(Ay,By),Cy);
+    int startx = std::min(std::min(Ax,Bx),Cx);
+    int endx = std::max(std::max(Ax,Bx),Cx);
+    int endy = std::min(std::min(Ay,By),Cy);
+
+
+    float denominateur = (((Bx-Ax)*(Cy-Ay))-((Cx-Ax)*(By-Ay)));
+
+
+    float coeff = 1./denominateur;
+
+       // cout << "Mon point de depart : " << startx << starty << "Mon point d'arrivÃ©e : " << endx << endy;
+    for(int i = starty; i>=endy; i--){
+        for(int j = startx; j<=endx;j++){
+            int Px = j;
+            int Py = i;
+                //image.set(Px, Py, red);
+            float u = (coeff*(Cy-Ay))*(Px-Ax)+(coeff*(Ax-Cx))*(Py-Ay);
+            float v = (coeff*(Ay-By))*(Px-Ax)+(coeff*(Bx-Ax))*(Py-Ay);
+            float w = 1-u-v;
+            if (u < 0 || v < 0 || w < 0){
+
+            }else{
+                Img->set(Px, Py, white);
+            }
+        }
+    }
+
+}
+
+void lineSweepingMethod(int Ax, int Ay, int Bx, int By, int Cx, int Cy){
+    if(Ay > By) {std::swap(Ay,By);std::swap(Ax,Bx);}
+    if(Ay > Cy) {std::swap(Ay,Cy);std::swap(Ax,Cx);}
+    if(By > Cy) {std::swap(By,Cy);std::swap(Bx,Cx);}
+
+    int bigHeight = Cy-Ay;
+
+
+    for(int y = Ay;y<By;y++){
+        int lowPartHeight = By-Ay;
+        float alpha = (float)(y-Ay)/bigHeight;
+        float beta  = (float)(y-Ay)/lowPartHeight; // be careful with divisions by zero
+        int Tmpx = Ax + (Cx-Ax)*alpha;
+        int Tmp2x = Ax + (Bx-Ax)*beta;
+        if (Tmpx>Tmp2x) std::swap(Tmpx,Tmp2x);
+        for (int j=Tmpx; j<=Tmp2x; j++) {
+            Img->set(j, y, white); // attention, due to int casts t0.y+i != A.y
+        }
+    }
+    for(int y = By;y<Cy;y++){
+        int highPartHeight = Cy-By;
+        float alpha = (float)(y-Ay)/bigHeight;
+        float beta  = (float)(y-By)/highPartHeight; // be careful with divisions by zero
+        int Tmpx = Ax + (Cx-Ax)*alpha;
+      //  int Tmpy = Ay + (Cy-Ay)*alpha;
+        int Tmp2x = Bx + (Cx-Bx)*beta;
+      //  int Tmp2y = Ay + (By-Ay)*beta;
+      //  Img->set(Tmpx, y, red);
+      //  Img->set(Tmp2x, y, white);
+        if (Tmpx>Tmp2x) std::swap(Tmpx,Tmp2x);
+        for (int j=Tmpx; j<=Tmp2x; j++) {
+            Img->set(j, y, white); // attention, due to int casts t0.y+i != A.y
+        }
+    }
+
+  /*  if (Ay==By && Ay==Cy) return; // i dont care about degenerate triangles
+    // sort the vertices, t0, t1, t2 lowerâˆ’toâˆ’upper (bubblesort yay!)
+    if (Ay>By) std::swap(t0, t1);
+    if (t0.y>t2.y) std::swap(t0, t2);
+    if (t1.y>t2.y) std::swap(t1, t2);
+    int total_height = t2.y-t0.y;
+    for (int i=0; i<total_height; i++) {
+        bool second_half = i>t1.y-t0.y || t1.y==t0.y;
+        int segment_height = second_half ? t2.y-t1.y : t1.y-t0.y;
+        float alpha = (float)i/total_height;
+        float beta  = (float)(i-(second_half ? t1.y-t0.y : 0))/segment_height; // be careful: with above conditions no division by zero here
+        Vec2i A =               t0 + (t2-t0)*alpha;
+        Vec2i B = second_half ? t1 + (t2-t1)*beta : t0 + (t1-t0)*beta;
+        if (A.x>B.x) std::swap(A, B);
+        for (int j=A.x; j<=B.x; j++) {
+            image.set(j, t0.y+i, color); // attention, due to int casts t0.y+i != A.y
+        }
+    }*/
+
+}
+void drawTriangle(int Ax, int Ay, int Bx, int By, int Cx, int Cy, TGAColor color){
+    line(Ax,Ay,Bx,By,*Img,color);
+    line(Ax,Ay,Cx,Cy,*Img,color);
+    line(Bx,By,Cx,Cy,*Img,color);
+
+}
 void parsefile(ifstream &file, ifstream &file2, std::vector<char> &tabvx, std::vector<char> &tabvy, std::vector<char> &tabvz, std::vector<int> &tabv, std::vector<std::vector<int> > &taballv){
 
     file.open("african_head.obj");
@@ -213,38 +305,19 @@ void parsefile(ifstream &file, ifstream &file2, std::vector<char> &tabvx, std::v
         issz >> nombre3;
         int Ax = taballv[nombre1-1][0];
         int Ay = taballv[nombre1-1][1];
+        int Az = taballv[nombre1-1][2];
         int Bx = taballv[nombre2-1][0];
         int By = taballv[nombre2-1][1];
+        int Bz = taballv[nombre2-1][2];
         int Cx = taballv[nombre3-1][0];
         int Cy = taballv[nombre3-1][1];
-        int starty = std::max(std::max(Ay,By),Cy);
-        int startx = std::min(std::min(Ax,Bx),Cx);
-        int endx = std::max(std::max(Ax,Bx),Cx);
-        int endy = std::min(std::min(Ay,By),Cy);
-        float denominateur = (((Bx-Ax)*(Cy-Ay))-((Cx-Ax)*(By-Ay)));
+        int Cz = taballv[nombre3-1][2];
 
+        barymetricFullMethod(Ax, Ay, Az, Bx, By, Bz, Cx, Cy, Cz);
+        //lineSweepingMethod(Ax,Ay,Bx,By,Cx,Cy);
+       // std::cout << "Triangle dessinÃ©";
+        drawTriangle(Ax,Ay,Bx,By,Cx,Cy,white);
 
-        float coeff = 1./denominateur;
-
-       // cout << "Mon point de depart : " << startx << starty << "Mon point d'arrivée : " << endx << endy;
-        for(int i = starty; i>=endy; i--){
-            for(int j = startx; j<=endx;j++){
-                int Px = j;
-                int Py = i;
-                //image.set(Px, Py, red);
-                float u = (coeff*(Cy-Ay))*(Px-Ax)+(coeff*(Ax-Cx))*(Py-Ay);
-                float v = (coeff*(Ay-By))*(Px-Ax)+(coeff*(Bx-Ax))*(Py-Ay);
-                float w = 1-u-v;
-                if (u < 0 || v < 0 || w < 0){
-
-                }else{
-                    Img->set(Px, Py, white);
-                }
-            }
-        }
-        line(taballv[nombre1-1][0],taballv[nombre1-1][1],taballv[nombre2-1][0],taballv[nombre2-1][1],*Img,white);
-        line(taballv[nombre2-1][0],taballv[nombre2-1][1],taballv[nombre3-1][0],taballv[nombre3-1][1],*Img,white);
-        line(taballv[nombre1-1][0],taballv[nombre1-1][1],taballv[nombre3-1][0],taballv[nombre3-1][1],*Img,white);
         sommet1.clear();
         sommet2.clear();
         sommet3.clear();
